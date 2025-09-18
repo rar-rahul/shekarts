043fd6516e10c09149759b73ca0df2f28ef7e7ff
rar-rahul <rahulraut430@gmail.com>
2025-09-18 09:49:18 -0400

    api signup

---
+++ pages/api/auth/sendotp.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export default async function apiHandler(req, res)  {
+    if (req.method === "POST") {
+    try {
+      const { phone } = req.body;
+
+      const apiKey = process.env.TWO_FACTOR_API_KEY;
+      const otpRes = await fetch(
+        `https://2factor.in/API/V1/${apiKey}/SMS/+91${phone}/AUTOGEN/SHEKARTSOTP`
+      );
+      const data = await otpRes.json();
+
+      res.status(200).json(data);
+    } catch (err) {
+      console.error("OTP API error:", err);
+      res.status(500).json({ error: "Server error" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
